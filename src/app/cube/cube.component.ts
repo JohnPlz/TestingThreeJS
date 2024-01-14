4ec03f0b5843efb0f46db4306aac489643fbc4ea
Ioannis Michail <ioannis@softwerkstatt.de>
2024-01-14 13:39:11 +0100

    small changes / added google fonts

--- src/app/cube/cube.component.ts
+++ src/app/cube/cube.component.ts
@@ -10,8 +10,8 @@ import * as THREE from "three";
   styleUrl: './cube.component.css'
 })
 export class CubeComponent implements AfterViewInit {
-  @ViewChild('canvas')
-  private canvasRef: ElementRef = {} as ElementRef;
+
+  @ViewChild('canvas') private canvasRef!: ElementRef;
 
   @Input() public rotationSpeedX: number = 0.002;
   @Input() public rotationSpeedY: number = 0.01;
@@ -19,20 +19,19 @@ export class CubeComponent implements AfterViewInit {
   @Input() public texture: string = 'assets/textures/texture_blue&grey.jpg';
 
   @Input() public cameraZ: number = 500;
-  @Input() public fieldOfView: number = 1;
+  @Input() public fieldOfView: number = 0.5;
   @Input('nearClipping') public nearClippingPlane: number = 1;
   @Input('farClipping') public farClippingPlane: number = 1000;
 
   private camera!: THREE.PerspectiveCamera;
   private loader = new THREE.TextureLoader();
   private geometry = new THREE.BoxGeometry(1, 1, 1);
-  private material = new THREE.MeshPhongMaterial({color: new THREE.Color('rgb(166,44,44)')});//new THREE.MeshBasicMaterial({map: this.loader.load(this.texture)});
+  private material = new THREE.MeshPhongMaterial({ color: new THREE.Color('rgb(166,44,44)') });//new THREE.MeshBasicMaterial({map: this.loader.load(this.texture)});
   private cube: THREE.Mesh = new THREE.Mesh(this.geometry, this.material);
   private renderer!: THREE.WebGLRenderer;
   private scene!: THREE.Scene;
 
   ngAfterViewInit(): void {
-      THREE.ColorManagement.enabled = true;
       this.createScene();
       this.startRenderingLoop();
   }
@@ -44,10 +43,10 @@ export class CubeComponent implements AfterViewInit {
   private createScene() {
     //scene
     this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color(0xffffff);
+    this.scene.background = new THREE.Color('rgb(224, 224, 224)');
     this.scene.add(this.cube);
     //add light
-    const light = new THREE.DirectionalLight(0xffffff, 4);
+    const light = new THREE.DirectionalLight(0xffffff, 6);
     light.position.set(0, 0, 100);
     this.scene.add(light);
     //camera
